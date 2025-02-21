5382f93b61c0a56aa3568d71d6158691b8069c17
Abdülsamet Rözgar <131825966+SametRozgar@users.noreply.github.com>
2025-02-22 02:28:55 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import Navbar from './components/Navbar'
 import Introduce from './components/Introduce'
 import Skils from './components/Skils'
 import Profile from './components/Profile'
-import Projects from './components/Projects'
+
+
 function App() {
  
 
@@ -13,7 +14,8 @@ function App() {
      <Introduce/>
      <Skils/>
      <Profile/>
-     <Projects/>
+   
+   
     </>
   )
 }
