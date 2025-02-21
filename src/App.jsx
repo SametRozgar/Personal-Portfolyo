6bf28796c2775d533b7db2c091804a3d5f4ab48e
Abdülsamet Rözgar <131825966+SametRozgar@users.noreply.github.com>
2025-02-22 01:43:30 +0300

    portfolyo unfunctional  view

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import Navbar from './components/navbar'
+import Introduce from './components/Introduce'
+import Skils from './components/Skils'
+import Profile from './components/Profile'
+import Projects from './components/Projects'
+function App() {
+ 
+
+  return (
+    <>
+     <Navbar/>
+     <Introduce/>
+     <Skils/>
+     <Profile/>
+     <Projects/>
+    </>
+  )
+}
+
+export default App
