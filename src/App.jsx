2f6dba95197cd42cc761655d6915380a2c1b8926
Abdülsamet Rözgar <131825966+SametRozgar@users.noreply.github.com>
2025-02-22 01:47:18 +0300

    vercel deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
 import Introduce from './components/Introduce'
 import Skils from './components/Skils'
 import Profile from './components/Profile'
