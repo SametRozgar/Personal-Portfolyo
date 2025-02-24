4192b4c04cfa18f9a3d801dd14306d2964f13278
Abdülsamet Rözgar <131825966+SametRozgar@users.noreply.github.com>
2025-02-24 23:50:00 +0300

    dark mode added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import "./navbar.css";
-
+import useDarkMode from "../hooks/useDarkMode";
 const Navbar = () => {
+  const [isDarkMode,setIsDarkMode]=useDarkMode();
   return (
     <div className="navbar">
       <div className="navbar-name">A</div>
@@ -9,6 +10,10 @@ const Navbar = () => {
         <a href="#" className="navbar-link-item">Skills</a>
         <a href="#" className="navbar-link-item">Projects</a>
         <a href="#" className="navbar-link-item">Hire me</a>
+
+        <button onClick={()=>setIsDarkMode(!isDarkMode)}>
+          sdfsd
+        </button>
         
       </div>
     </div>
