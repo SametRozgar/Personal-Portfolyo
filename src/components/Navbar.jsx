545a33d7008cc5403d59bd7c65a21c91bda8d85a
Abdülsamet Rözgar <131825966+SametRozgar@users.noreply.github.com>
2025-02-25 23:38:25 +0300

    translate start

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./navbar.css";
 import useDarkMode from "../hooks/useDarkMode";
+import ceviri from "../data/translations.json"
 const Navbar = () => {
   const [isDarkMode,setIsDarkMode]=useDarkMode();
+  const [language,setLanguage]=useState("en")
+  const [content,setContent]=useState(ceviri[language])
+  
+  useEffect(()=>
+  {
+  setContent(ceviri[language])
+  },[language])
+
+  const dildegistir =()=>
+  {
+    setLanguage("tr")
+  }
   return (
     <div className="navbar">
       <div className="navbar-name">A</div>
       <div className="navbar-links">
-        <a href="#" className="navbar-link-item">Skills</a>
+        <a href="#" className="navbar-link-item">{content.skils}</a>
         <a href="#" className="navbar-link-item">Projects</a>
         <a href="#" className="navbar-link-item">Hire me</a>
 
         <button onClick={()=>setIsDarkMode(!isDarkMode)}>
           sdfsd
+        </button>
+
+        <button onClick={dildegistir}>
+          dili değistir
         </button>
         
       </div>
---
+++ src/context/LanguageContexts.jsx
@@ -0,0 +1,9 @@
+import { createContext } from "react";
+
+
+const languageContext=createContext();
+const [language,setLanguage]=useState("en")
+export const LanguageContexts =()=>
+{
+
+}
